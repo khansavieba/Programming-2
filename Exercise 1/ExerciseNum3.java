611307adb56e9295cefe8b31b0d69037ff842433
khansavieba <61281276+khansavieba@users.noreply.github.com>
2020-03-05 15:53:58 +0800

    Update ExerciseNum3.java

--- Exercise 1/ExerciseNum3.java
+++ Exercise 1/ExerciseNum3.java
@@ -1,21 +1,20 @@
 import java.util.*;
-public class ExerciseNum3 {
+public class ExerciseNum4 {
    public static void main(String[] args){
    Scanner sc = new Scanner(System.in);
-   System.out.println("Enter 4-digit number : ");
-   String number = sc.nextLine();
-   for(int i = 0; i<4 ; i++){
-   System.out.println(number.charAt(i));
- 
+   System.out.println("Enter 4-digit number : "); //asking for user input 
+   String number = sc.nextLine(); //assigning user input into variable number 
+   String number1; 
+   int sum = 0;
    
-   }
-   int numberX = Integer.parseInt(number);
-   int count = 0;
-   while (numberX > 0){
-   count += numberX;
-   }
+      for(int i = 0; i<number.length() ; i++){
+         System.out.println(number.charAt(i)); //printing number line by line 
+         number1 = String.valueOf(number.charAt(i)); //convert character to string 
+         sum = sum + Integer.parseInt(number1);   //initialize and calculating the total 
+         }
    
-   System.out.println(count);
+   
+   System.out.println("The total of the digits = " + sum ); //print the total 
  
    
    
