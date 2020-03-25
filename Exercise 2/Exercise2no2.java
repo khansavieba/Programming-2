94c107e0b67e3e542169ffc7c9eb1a424f5b2bdd
khansavieba <61281276+khansavieba@users.noreply.github.com>
2020-03-26 00:48:11 +0700

    Update Exercise2no2.java

--- Exercise 2/Exercise2no2.java
+++ Exercise 2/Exercise2no2.java
@@ -1,29 +1,38 @@
 /**
 2.	Write a program called CountA that accepts a String parameter and display the number of times the character 'A' is found in the string.
 **/
+package com.company;
+
+///**
+// 2.	Write a program called CountA that accepts a String parameter and display the number of times the character 'A' is found in the string.
+// **/
 import java.util.Scanner;
-public class Exercise2no2{ 
+public class Exercise2no2{
 
-      public static void main(String args[]) 
-    { 
-        String str= "COLLEGE OF ART AND SCIENCES"; 
-        char a = 'A'; 
-        System.out.println(str);
-        System.out.println("number of time the character 'A' is found : " + countA(str, a)); 
-    } 
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence : ");
+        String str = sc.nextLine();
+        char a = 'A';
+        char a2 = 'a';
+        System.out.println("number of time the character 'A' is found : " + countA(str, a, a2));
+    }
 
-   
-    public static int countA(String cas, char a) 
-    { 
-        int count = 0; 
-  
-        for (int i=0; i<cas.length(); i++) 
-        { 
-            // checking character in string 
-            if (cas.charAt(i) == a) 
-            count++; 
-        }  
-        return count; 
-    } 
-      
-   } 
+
+    public static int countA(String str, char a, char a2)
+    {
+        int count = 0;
+
+        for (int i=0; i<str.length(); i++)
+        {
+            // checking character in string
+            if (str.charAt(i) == a)
+                count++;
+            if (str.charAt(i) == a2)
+                count++;
+        }
+        return count;
+    }
+
+}
