4e2f1b10f835d160751e792363a7e7b692c65251
khansavieba <61281276+khansavieba@users.noreply.github.com>
2020-03-26 19:17:18 +0700

    Update Exercise2No1.java

--- Exercise 2/Exercise2No1.java
+++ Exercise 2/Exercise2No1.java
@@ -14,14 +14,14 @@ import java.lang.Math;
 import java.util.*;
 public class Exercise2No1 {
    public static void main (String args[]){
-   Scanner sc1 = new Scanner(System.in);
-   System.out.println("Enter a number: ");
-   double num = sc1.nextDouble(); //assign user's input into double 
-   double square = Math.pow(num,2); //calculate the number to the power of 2 
-   double cube = Math.pow(num,3); //calculate the number to the power of 3 
-   System.out.printf("%-15s%-15s%-15s","Number","Square","Cube");
-   System.out.printf("\n%-15s%-15s%-15s",num,square,cube);
 
+
+   System.out.println("Number\tSquare\tCube");
+   
+   for(int i=0;i<=10;i++){
+          System.out.println(i+"\t\t\t"+(int)Math.pow(i,2)+"\t\t\t"+(int)Math.pow(i,3));
+          
    }
 
+}
 }
